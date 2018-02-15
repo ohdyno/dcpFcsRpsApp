94e4a4b9169c82b7afd558322a2a671cb1041c3b
Xing Zhou <xzhou@pivotal.io>
2018-02-15 14:15:31 -0500

    Implement feature for playing a single round in domain

---
+++ rps/spec/rpsSpec.js
@@ -0,0 +1,98 @@
+class RPS {
+    play(p1, p2, ui) {
+        if (this.invalidInput(p1) || this.invalidInput(p2)) {
+            ui.invalid();
+            return
+        }
+        if (p1 === p2) {
+            ui.tie();
+            return
+        }
+        if (p1 === 'rock' && p2 === 'scissors' ||
+            p1 === 'scissors' && p2 === 'paper' ||
+            p1 === 'paper' && p2 === 'rock') {
+            ui.player1Wins();
+            return
+        }
+        ui.player2Wins()
+    }
+
+    invalidInput(p1) {
+        return p1 !== 'rock' && p1 !== 'scissors' && p1 !== 'paper';
+    }
+}
+
+describe("RPS", function () {
+    const rps = new RPS();
+    describe("play a single round", function () {
+        describe("for valid inputs", function () {
+            describe("and player 1 wins", function () {
+                let uiSpy = {
+                    player1Wins: jasmine.createSpy('player1WinsSpy')
+                };
+
+                it("tells the ui for rock vs. scissors", function () {
+                    rps.play("rock", "scissors", uiSpy);
+                    expect(uiSpy.player1Wins).toHaveBeenCalled()
+                });
+
+                it("tells the ui for scissors vs. paper", function () {
+                    rps.play("scissors", "paper", uiSpy);
+                    expect(uiSpy.player1Wins).toHaveBeenCalled()
+                });
+
+                it("tells the ui for paper vs. rock", function () {
+                    rps.play("paper", "rock", uiSpy);
+                    expect(uiSpy.player1Wins).toHaveBeenCalled()
+                });
+            });
+
+            describe("and player 2 wins", function () {
+                let uiSpy = {
+                    player2Wins: jasmine.createSpy('player2WinsSpy')
+                };
+
+                it("tells the ui for scissors vs. rock", function () {
+                    rps.play("scissors", "rock", uiSpy);
+                    expect(uiSpy.player2Wins).toHaveBeenCalled()
+                });
+
+                it("tells the ui for rock vs. paper", function () {
+                    rps.play("rock", "paper", uiSpy);
+                    expect(uiSpy.player2Wins).toHaveBeenCalled()
+                });
+
+                it("tells the ui for paper vs. scissors", function () {
+                    rps.play("paper", "scissors", uiSpy);
+                    expect(uiSpy.player2Wins).toHaveBeenCalled()
+                });
+            });
+
+            describe("and tie", function () {
+                let uiSpy = {
+                    tie: jasmine.createSpy('tieSpy')
+                };
+
+                it("tells the ui for the same throws", function () {
+                    rps.play("rock", "rock", uiSpy);
+                    expect(uiSpy.tie).toHaveBeenCalled()
+                });
+            });
+        });
+        describe("for invalid inputs", function () {
+            let uiSpy = {
+                invalid: jasmine.createSpy('invalidSpy')
+            };
+
+            it('tells the ui for player 1 invalid throw', function () {
+                rps.play('sailboat', 'rock', uiSpy);
+                expect(uiSpy.invalid).toHaveBeenCalled()
+            });
+
+            it('tells the ui for player 2 invalid throw', function () {
+                rps.play('rock', 'sailboat', uiSpy);
+                expect(uiSpy.invalid).toHaveBeenCalled()
+            });
+        });
+    });
+});
