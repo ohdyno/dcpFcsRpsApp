767765bb33df4680864e67775d80a6f2667f282e
Xing Zhou <xzhou@pivotal.io>
2018-02-15 14:57:41 -0500

    Extract rps into its own object

--- rps/spec/rpsSpec.js
+++ rps/spec/rpsSpec.js
@@ -1,26 +1,4 @@
-class RPS {
-    play(p1, p2, ui) {
-        if (this.invalidInput(p1) || this.invalidInput(p2)) {
-            ui.invalid();
-            return
-        }
-        if (p1 === p2) {
-            ui.tie();
-            return
-        }
-        if (p1 === 'rock' && p2 === 'scissors' ||
-            p1 === 'scissors' && p2 === 'paper' ||
-            p1 === 'paper' && p2 === 'rock') {
-            ui.player1Wins();
-            return
-        }
-        ui.player2Wins()
-    }
-
-    invalidInput(p1) {
-        return p1 !== 'rock' && p1 !== 'scissors' && p1 !== 'paper';
-    }
-}
+const RPS = require('../src/rps');
 
 describe("RPS", function () {
     const rps = new RPS();
