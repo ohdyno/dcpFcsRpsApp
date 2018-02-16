026b08c25fb413d8964b2a8b43c1f6c781cfe62c
Xing Zhou <xzhou@pivotal.io>
2018-02-16 12:01:10 -0500

    Refactor history spec

--- rps/spec/historySpec.js
+++ rps/spec/historySpec.js
@@ -1,12 +1,16 @@
 const RPS = require('../src/rps');
 
 describe("history", function () {
+    const rps = new RPS();
     describe("for when no rounds have been played", function () {
-        it("tells the UI that no rounds have been played", function () {
-            const rps = new RPS();
-            let uiSpy = {
+        let uiSpy;
+        beforeEach(function () {
+            uiSpy = {
                 noRounds: jasmine.createSpy('noRoundsSpy')
             };
+        });
+
+        it("tells the UI that no rounds have been played", function () {
             rps.getHistory(uiSpy);
             expect(uiSpy.noRounds).toHaveBeenCalled()
         });
