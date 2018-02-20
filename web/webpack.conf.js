d852360113317f61874ab4e5c8edffe36c50ef60
Xing Zhou <xzhou@pivotal.io>
2018-02-20 13:36:12 -0500

    Automate the UI tests with Karma

---
+++ web/webpack.conf.js
@@ -0,0 +1,14 @@
+module.exports = {
+    watch: false,
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+}
